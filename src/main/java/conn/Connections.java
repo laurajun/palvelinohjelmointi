b8079313cae48eaa602d8f300093c0e2229ff0ab
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-04-17 21:05:49 +0300

    hikari config

--- src/main/java/conn/Connections.java
+++ src/main/java/conn/Connections.java
@@ -10,23 +10,26 @@ import com.zaxxer.hikari.HikariDataSource;
 
 public class Connections {
 
-private static DataSource pool=null;
+	private static DataSource pool=null;
 	
 	public static Connection getConnection() throws SQLException {
-		if (pool!=null) {
+		if (pool != null) {
 			return pool.getConnection();
 		}
+				
 		// The configuration object specifies behaviors for the connection pool.
 		HikariConfig config = new HikariConfig();
 		 // Configure which instance and what database user to connect with.
 		config.setDriverClassName(System.getProperty("drivername")); // see appengine-web.xml
 		config.setJdbcUrl("jdbc:mysql://"+System.getProperty("sqlhostname")+":"+System.getProperty("sqlserverport")+"/"+System.getProperty("sqldatabasename")); // see appengine-web.xml
-		config.setUsername(System.getProperty("localusername")); // see appengine-web.xml
-		config.setPassword(System.getProperty("localpassword")); // see appengine-web.xml
+		//config.setJdbcUrl("jdbc:mysql://localhost:3306/Databasename"); // see appengine-web.xml
+		config.setUsername(System.getProperty("sqlusername")); // see appengine-web.xml
+		config.setPassword(System.getProperty("sqlpassword")); // see appengine-web.xml
 		
 		  // Initialize the connection pool using the configuration object.
 		pool = new HikariDataSource(config);
 		  
 		return pool.getConnection();
+		
 	}
 }

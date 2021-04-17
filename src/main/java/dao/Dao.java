bae9a6d32cc003d7d286e131a85d869bfa58b7d7
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-04-17 21:09:34 +0300

    Initial commit for Dao class

---
+++ src/main/java/dao/Dao.java
@@ -0,0 +1,25 @@
+package dao;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import conn.Connections;
+
+public class Dao {
+	private Connection conn;
+	public Dao() {
+		try {
+			conn=Connections.getConnection();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void close() {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

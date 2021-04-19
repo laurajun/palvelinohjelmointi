9fcaf5c6a1f74ba7df7343d8e3f86fa381deb17c
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-04-19 19:37:57 +0300

    Working example of ListOptions

--- src/main/java/app/ListOptions.java
+++ src/main/java/app/ListOptions.java
@@ -1,11 +1,9 @@
 package app;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -13,6 +11,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+
+import data.Vaihtoehto;
 
 
 /**
@@ -29,6 +31,14 @@ public class ListOptions extends HttpServlet {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+	public void printOptionList(PrintWriter out, ArrayList<Vaihtoehto> list) {
+		out.println("<ul>");
+		for (Vaihtoehto g:list) {
+			out.println("<li>"+g);
+		}
+		out.println("</ul>");
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -37,6 +47,8 @@ public class ListOptions extends HttpServlet {
 		response.setContentType("text/html");
 		PrintWriter out=response.getWriter();
 
+
+		
 		/*
 		 * With a RequestDispatcher object is the htmlstart.html file included to this servlet
 		 */
@@ -46,73 +58,26 @@ public class ListOptions extends HttpServlet {
 		/* 
 		 * Connection is the object to keep the connection to the database
 		 */
-		Connection conn=null;
-		try {
-			/* 
-			 * As not using the database connection pool, can the connection be caught like this
-			 * At first, this loads MySQL driver to the container
-			 * and secondly getting connection to the mysql server and database gamedb at the localhost
-			 * Port number is 3306 by default
-			 */
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String sqlhostname = System.getProperty("sqlhostname");
-			String serverport = System.getProperty("sqlserverport");
-			String database = System.getProperty("sqldatabasename");
-			String username = System.getProperty("sqlusername");
-			String password = System.getProperty("sqlpassword");
-			String url = "jdbc:mysql://"+sqlhostname+":"+serverport+"/"+database;
-			conn=java.sql.DriverManager.getConnection(url,username,password);
-			System.out.println("Database connection established");
-		
-			Statement stmt=conn.createStatement();
-			ResultSet rs=stmt.executeQuery("select * from vaihtoehdot");
-			out.println("<ol>");
-			/*
-			 * Looping through all the rows and printing the values of the columns breed and weight
-			 * All the data in the columns can be read as String (getString). If there is a need to
-			 * read integers or floats, can getInt or getFloat be used. And there are quite many 
-			 * different functions to retrieve data.
-			 */
-			while (rs.next()){
-				out.println("<li>"+rs.getInt("id")+" ("+rs.getString("vaihtoehto")+")");
-			}	  
-		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
-			System.err.println("Cannot connect to database server");
-            System.err.println(e.getMessage());
-			e.printStackTrace();
-		}
-		out.println("<a href='./addgame.html'>Back to form</a>");
+			//Connection conn=null;
 
-		/*
-		 * With a RequestDispatcher object is the htmlend.html file included to this servlet
-		 */
-		rd=request.getRequestDispatcher("htmlend.html");
-		rd.include(request,  response);;
-  }
-
-  /**
-   * 
-   * @param conn Connection to the database
-   * @param stmt Object to execute the sql commands
-   * @param breed breed of the game
-   * @param weight weight of the game
-   * @return number of inserted rows
-   * @throws SQLException
-   */
-	private int saveTheGame(Connection conn, Statement stmt, String breed, String weight) throws SQLException {
-		// TODO Auto-generated method stub
-		/*
-		 * Executing insert statement
-		 * Though the weight column in the database is of type decimal, a String can be inserted
-		 */
-		int count=stmt.executeUpdate("insert into gametable(breed, weight) values('"+breed+"', "+weight+")");
-		return count; //Return the number of inserted rows (here it is 1)
-	}
 	
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+			try {
+				Dao dao = new Dao();
+				dao.openDataBase();
+				ArrayList<Vaihtoehto> list=dao.readAllOption();
+				printOptionList(out, list);
+				dao.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
+	
+  }
+
+ 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);

787c54efea251b015db7c843e0243fe983a75d4d
laurajun <laura1738@student.hamk.fi>
2021-04-20 18:38:52 +0300

    Created Claim class

---
+++ src/main/java/data/Claim.java
@@ -0,0 +1,24 @@
+package app;
+
+public class Claim {
+
+        private int id;
+        private String vaittama;
+
+        public int getId() {
+		return id;
+	}
+        public void setId(int id) {
+		this.id = id;
+	}
+	public String getClaim() {
+		return vaittama;
+	}
+        public void setClaim(String vaittama) {
+		this.vaittama = vaittama;
+        }
+
+        public String toString() {
+		return id+" "+vaittama;
+	}
+}

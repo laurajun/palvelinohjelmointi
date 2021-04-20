524f56b16219838eb06343555705c0b833c4b2a0
laurajun <laura1738@student.hamk.fi>
2021-04-20 18:48:20 +0300

    Added Connections class and created conn package, make alternative dao class for testing

---
+++ src/main/java/conn/Connections.java
@@ -0,0 +1,33 @@
+package conn;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class Connections {
+	
+	private static DataSource pool=null;
+	
+	public static Connection getConnection() throws SQLException {
+		if (pool!=null) {
+			return pool.getConnection();
+		}
+		// The configuration object specifies behaviors for the connection pool.
+		HikariConfig config = new HikariConfig();
+		 // Configure which instance and what database user to connect with.
+		config.setDriverClassName(System.getProperty("drivername")); // see appengine-web.xml
+		config.setJdbcUrl(System.getProperty("sqlhostname")+System.getProperty("databasename")); // see appengine-web.xml
+		config.setUsername(System.getProperty("sqlhostname")); // see appengine-web.xml
+		config.setPassword(System.getProperty("sqlpassword")); // see appengine-web.xml
+		
+		  // Initialize the connection pool using the configuration object.
+		pool = new HikariDataSource(config);
+		  
+		return pool.getConnection();
+	}
+
+}

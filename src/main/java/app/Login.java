16f5f813d1ecf6dc18c983f55cbc038a4697b03b
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-04-20 09:14:37 +0300

    Reconfigure Login servlet

--- src/main/java/app/Login.java
+++ src/main/java/app/Login.java
@@ -1,11 +1,19 @@
 package app;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Login
@@ -34,8 +42,49 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		PrintWriter out=response.getWriter();
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        
+        String sqlhostname = System.getProperty("sqlhostname");
+		String serverport = System.getProperty("sqlserverport");
+		String database = System.getProperty("sqldatabasename");
+		String sqlusername = System.getProperty("sqlusername");
+		String sqlpassword = System.getProperty("sqlpassword");
+		String url = "jdbc:mysql://"+sqlhostname+":"+serverport+"/"+database;
+	 
+		try{
+			String destPage = "index.jsp";
+		    Class.forName("com.mysql.jdbc.Driver");
+		    Connection connection = DriverManager.getConnection(url, sqlusername, sqlpassword);
+		    Statement st= connection.createStatement();
+		    ResultSet rs=st.executeQuery("select * from logins where username='"+username+"' and password='"+password+"'");
+		    while(rs.next())
+		    {
+			    String fullname = rs.getString("fullname");
+			    if(rs.getString("password").equals(password)&&rs.getString("username").equals(username))
+			    {
+			    
+				    HttpSession session = request.getSession();
+			        session.setAttribute("fullname", fullname);
+			        destPage = "home.jsp";
+			    }
+			    else{
+			    	HttpSession session = request.getSession();
+			    	String message = "Invalid username and/or password";
+			        session.setAttribute("message", message);
+			        destPage = "index.jsp";
+			        
+			    }
+			    
+		    }
+		    RequestDispatcher dispatcher = request.getRequestDispatcher(destPage);
+	        dispatcher.forward(request, response);
+	    }
+	    catch (Exception e) {
+	    e.printStackTrace();
+	    }
+		
 	}
 
 }
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Vaalikone - Logged in</title>
+</head>
+<body>
+    <div style="text-align: center">
+        <h1>Tervetuloa Vaalikoneeseen!</h1>
+        <b>${fullname}</b>
+        <br><br>
+        <a href="/logout">Logout</a>
+    </div>
+</body>
+</html>

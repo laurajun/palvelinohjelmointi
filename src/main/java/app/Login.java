b28a7d7754d4dd32b15787a484530e0cfae75d36
tkomulai <54975752+tkomulai@users.noreply.github.com>
2021-04-21 09:22:01 +0300

    Added comment for the video

--- src/main/java/app/Login.java
+++ src/main/java/app/Login.java
@@ -42,6 +42,7 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Just a comment for the video purposes
 		PrintWriter out=response.getWriter();
 		String username = request.getParameter("username");
         String password = request.getParameter("password");
